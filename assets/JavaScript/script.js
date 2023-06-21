49ed8f90ecfe0d9cbf7e968c2b12f2555af9cfd3
HND <hans.doderlein99@gmail.com>
2023-06-21 10:15:10 -0400

    added hour and minute timestamp to calandar

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -5,7 +5,7 @@ $(function () {
   dayjs.extend(window.dayjs_plugin_advancedFormat);
 
   // displays current day
-  $("#currentDay").text(dayjs().format("dddd, MMMM Do"));
+  $("#currentDay").text(dayjs().format("dddd, MMMM Do. h:mm a"));
 
   //adds class based on current time
   $.each(timeblock, (index) => {
